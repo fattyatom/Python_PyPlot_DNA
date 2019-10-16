3cb260c9f3c3d6dad1462355c06ade4aa876e739
b9062344 <c.chan8@mewcastle.ac.uk>
2019-10-16 09:57:46 +0100

    adding a command line done, reverted some steps

--- uniplot/cli.py
+++ uniplot/cli.py
@@ -4,4 +4,4 @@ from Bio import SeqIO
 def cli():
     handle = gzip.open("uniprot_receptor.xml.gz")
     for record in SeqIO.parse(handle, "uniprot-xml"):
-        print(record)
+        print(record.name)
