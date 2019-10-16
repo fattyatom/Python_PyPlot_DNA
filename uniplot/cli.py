877a5363fa78f4e3cd7b13d39b1faf0243abdc43
b9062344 <c.chan8@mewcastle.ac.uk>
2019-10-16 10:01:31 +0100

    argparse dump / list working

--- uniplot/cli.py
+++ uniplot/cli.py
@@ -1,7 +1,29 @@
+import argparse
 import gzip
 from Bio import SeqIO
 
-def cli():
+def dump(args):
+    handle = gzip.open("uniprot_receptor.xml.gz")
+    for record in SeqIO.parse(handle, "uniprot-xml"):
+        print(record)
+
+def names(args):
     handle = gzip.open("uniprot_receptor.xml.gz")
     for record in SeqIO.parse(handle, "uniprot-xml"):
         print(record.name)
+
+def cli():
+    ## Create a new parser
+    parser = argparse.ArgumentParser(prog="uniplot")
+
+    subparsers = parser.add_subparsers(help="Sub Command Help")
+
+    ## Add subparsers
+    subparsers.add_parser("dump").set_defaults(func=dump)
+    subparsers.add_parser("list").set_defaults(func=names)
+
+    ## Parse the command line
+    args = parser.parse_args()
+
+    ## Take the func argument, which points to our function and call it.
+    args.func(args)
