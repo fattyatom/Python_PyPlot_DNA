538364844145a1895a84450ff84b8d08f11e307a
b9062344 <c.chan8@mewcastle.ac.uk>
2019-10-29 11:20:30 +0000

    Uniprot File Configurable

--- uniplot/cli.py
+++ uniplot/cli.py
@@ -1,34 +1,37 @@
-import argparse, pathlib
+import argparse
 from . import parse
 from . import analysis
 from . import plot
 
-LOC='uniprot_receptor.xml.gz'
 
 def dump(args):
     """Prints the records in the uniprot_seqrecords in the parse.py file."""
-    for record in parse.uniprot_seqrecords(LOC):
+    for record in parse.uniprot_seqrecords(args.customfile):
         print(record)
 
 def names(args):
     """Prints the names in the records in the dump of uniprot_seqrecords."""
-    for record in parse.uniprot_seqrecords(LOC):
+    for record in parse.uniprot_seqrecords(args.customfile):
         print(record.name)
 
 def average(args):
     """Prints the average length of proteins."""
     print("Average Length is {}".format(
-        analysis.average_len(parse.uniprot_seqrecords(LOC))))
+        analysis.average_len(parse.uniprot_seqrecords(args.customfile))))
 
 def plot_average_by_taxa(args):
     """Generates a table of various types of average protein lengths."""
-    av = analysis.average_len_taxa(parse.uniprot_seqrecords(LOC))
+    av = analysis.average_len_taxa(parse.uniprot_seqrecords(args.customfile))
     plot.plot_bar_show(av)
 
 def cli():
     """Handles all the argument parsers required in the table."""
     ## Create a new parser
     parser = argparse.ArgumentParser(prog="uniplot", description="A python program of discovering DNA sequences")
+
+    parser.add_argument('--customfile',  default='uniprot_receptor.xml.gz', action='store', help='Type custom protein DB file path at prompt for custom UniProt file')
+
+    subparsers = parser.add_subparsers(help="Sub Commands Help")
 
     ## Add subparsers
     subparsers.add_parser("dump", help='Print all values in the database').set_defaults(func=dump)
@@ -42,3 +45,5 @@ def cli():
 
     ## Take the func argument, which points to our function and call it.
     args.func(args)
+
+# H:/PyCharmProjects/practical-2/resources/uniprot_sprot_small.xml.gz
